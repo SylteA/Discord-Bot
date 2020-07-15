7dd55708f6f8e3c004fed929a118716bdf5f2986
Sylte <sylteandreas7@gmail.com>
2020-07-15 23:46:28 +0200

    fix..

--- cogs/tags.py
+++ cogs/tags.py
@@ -124,7 +124,7 @@ class TagCommands(commands.Cog, name="Tags"):
 
         if not records:
             return await ctx.send("No tags found that has the term in it's name")
-
+        count = len(records)
         records = "\n".join([record["name"] for record in records])
 
-        await ctx.send(f"**{len(records)} tags found with search term on this server.**```\n{records}\n```")
+        await ctx.send(f"**{count} tags found with search term on this server.**```\n{records}\n```")
