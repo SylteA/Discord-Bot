5e7e9f5d69175c5e1855ef3ac6e91cddaa6371a8
Takos <59511469+takos22@users.noreply.github.com>
2020-07-16 08:50:05 +0200

    Add back t.tag all

--- cogs/tags.py
+++ cogs/tags.py
@@ -82,6 +82,26 @@ class TagCommands(commands.Cog, name="Tags"):
             await ctx.send(page)
 
     @tag.command()
+    async def all(self, ctx: commands.Context):
+        """List all existing tags alphabetically ordered and sends them in DMs."""
+        records = await self.bot.db.fetch(
+            """SELECT name FROM tags WHERE guild_id = $1 ORDER BY name""",
+            ctx.guild.id
+        )
+        
+        if not records:
+            return await ctx.send("This server doesn't have any tags.")
+
+        pager = commands.Paginator()
+        pager.add_line(f"**{len(records)} tags found on this server.**")
+
+        for record in records:
+            pager.add_line(line=record["name"])
+
+        for page in pager.pages:
+            await ctx.author.send(page)
+
+    @tag.command()
     @is_engineer_check()
     async def edit(self, ctx, name: str, *, text: str):
         """Edit a tag"""
