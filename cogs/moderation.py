5e2eb0c5ba6779c12c4982df71b06bed9625e39f
Takos <59511469+takos22@users.noreply.github.com>
2020-08-30 23:45:36 +0200

    Add Moderation cog and report command

---
+++ cogs/moderation.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Moderation(bot=bot))
+
+
+class Moderation(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @property
+    def report_channel(self):
+        return self.bot.get_channel(749742688521158656)
+
+    @commands.command("report")
+    @commands.dm_only()
+    async def report(self, ctx, user: discord.User, reason: str):
+        await ctx.em(title=f"**Thank you for reporting {user.mention} for:**", description=reason)
+        em = discord.Embed(title=f"Report {user.mention} for:**", description=reason)
+        em.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
+        await self.report_channel.send(embed=em)
