9ff3cb58752591945f58cb53782b45a412f74ac1
Takos <59511469+takos22@users.noreply.github.com>
2020-08-31 14:09:09 +0200

    Added docstring to report command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -17,6 +17,7 @@ class Moderation(commands.Cog):
     @commands.command("report")
     @commands.dm_only()
     async def report(self, ctx, user: discord.User, reason: str):
+        """Report a user to staff for a reason."
         await ctx.em(title=f"**Thank you for reporting {user} for:**", description=reason)
         em = discord.Embed(title=f"**Report {user} for:**", description=reason)
         em.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
