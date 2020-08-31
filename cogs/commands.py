3dd06db1bf230290d9a32511790e23bb8064b094
12944qwerty <qwerty12944qwerty@gmail.com>
2020-08-31 17:12:08 -0500

    Update commands.py

--- cogs/commands.py
+++ cogs/commands.py
@@ -357,14 +357,14 @@ class Commands(commands.Cog):
             await ctx.send(embed=em)
     
     @commands.command(name="suggest", aliases=["poll"])
-	async def suggestion(self, ctx, *, suggestion:str):
-		"""Make a poll/suggestion"""
-		await ctx.message.delete()
-		em = discord.Embed(title=suggestion)
-		em.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-		msg = await ctx.send(embed=em)
-		await msg.add_reaction('👍')
-		await msg.add_reaction('👎')
+    async def suggestion(self, ctx, *, suggestion:str):
+        """Make a poll/suggestion"""
+        await ctx.message.delete()
+        em = discord.Embed(title="Poll", description=ctx.message.content)
+        em.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        msg = await ctx.send(embed=em)
+        await msg.add_reaction('👍')
+        await msg.add_reaction('👎')
 
     async def build_docs_lookup_table(self, page_types):
         cache = {}
