3dad943b45da63a35b3649892a94a813e5a1b1e5
12944qwerty <qwerty12944qwerty@gmail.com>
2020-09-01 19:04:08 -0500

    doesn't let bot get reported and better embed

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime.datetime import now
 
 
 def setup(bot: commands.Bot):
@@ -18,8 +19,14 @@ class Moderation(commands.Cog):
     @commands.guild_only()
     async def report(self, ctx, user: discord.User, *, reason: str):
         """Report a user to staff for a reason."""
+        if user.bot:
+            return await ctx.send("You cannot report a bot")
         await ctx.em(title=f"**Thank you for reporting {user} for:**", description=reason)
-        em = discord.Embed(title=f"**Report {user} for:**", description=reason)
-        em.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        em = discord.Embed(title=f"**Report**", color=discord.Colour.red())
+        em.add_field(name="Reported User", value=user.display_name)
+        em.add_field(name="Reporter", value=ctx.author.display_name)
+        em.add_field(name="Channel", value=ctx.message.channel.mention)
+        em.add_field(name="Reason", value=reason)
+        em.set_footer(text=f"{ctx.author.display_name} | {str(now())}", icon_url=ctx.author.avatar_url)
         await self.report_channel.send(embed=em)
 
