9dfde78e5b071f586fc5d00c228d6267a8ffd2cd
Takos <59511469+takos22@users.noreply.github.com>
2020-09-02 22:27:39 +0200

    Delete message

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -19,16 +19,16 @@ class Moderation(commands.Cog):
     @commands.command("report")
     @commands.guild_only()
     @commands.cooldown(1, 15, commands.BucketType.channel)
-    async def report(self, ctx, user: discord.Member, *, reason: str):
+    async def report(self, ctx, member: discord.Member, *, reason: str):
         """Report a user to staff for a reason."""
-        if user.bot:
+        if member.bot:
             ctx.command.reset_cooldown(ctx)
             return await ctx.send("You can't warn a bot <:mhm:687726663676592145>")
 
         embed = discord.Embed(title=f"Report", timestamp=datetime.datetime.utcnow(), inline=False)
-        embed.add_field(name="Reported User", value=f"{user.mention} ({user.id})", inline=False)
+        embed.add_field(name="Reported Member", value=f"{member.mention} ({member.id})", inline=False)
         embed.add_field(name="Reporter", value=f"{ctx.author.mention} ({ctx.author.id})", inline=False)
         embed.add_field(name="Reason", value=reason, inline=False)
 
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed, delete_after=10.0)
         await self.report_channel.send(embed=embed)
