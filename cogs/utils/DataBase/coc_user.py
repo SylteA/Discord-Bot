3531a61d0613a0df33e60d113c5f7f46f6f72ff0
Takos22 <tora.akos@gmail.com>
2020-09-06 20:09:10 +0200

    Update CocUser model

--- cogs/utils/DataBase/coc_user.py
+++ cogs/utils/DataBase/coc_user.py
@@ -12,3 +12,13 @@ class CocUser:
     def __repr__(self):
         return (f"<CocUser discord_id={repr(self.discord_id)} "
                 f"coc_id={repr(self.coc_id)} wins={repr(self.wins)}>")
+
+    async def post(self):
+        query = """INSERT INTO coc_users ( discord_id, coc_id, wins )
+                   VALUES ( $1, $2, $3 )"""
+        await self.bot.db.execute(query, self.discord_id, self.coc_id, self.wins)
+
+    async def add_win(self):
+        self.wins += 1
+        query = """UPDATE coc_users SET wins = wins + 1 WHERE coc_id = $1"""
+        await self.bot.db.execute(query, self.coc_id)
