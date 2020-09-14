95d59607c76110421095624450408a128de55705
Shubhaankar-Sharma <shubhaankar@hotmail.com>
2020-09-14 22:33:17 +0530

    added thumbs up reaction for every message in project showcase and ad… (#100)
    * added thumbs up reaction for every message in project showcase and added tabulate in requirements as it was missing
    
    * Update main.py
    
    Co-authored-by: Sylte <49946393+SylteA@users.noreply.github.com>

--- main.py
+++ main.py
@@ -68,11 +68,15 @@ class Tim(commands.AutoShardedBot):
 
     async def on_message(self, message):
         await self.wait_until_ready()
+
         if message.author.bot:
             return
+
         print(f"{message.channel}: {message.author}: {message.clean_content}")
+
         if not message.guild:
             return
+
         await self.process_commands(message)
 
     async def process_commands(self, message):
