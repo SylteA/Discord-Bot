71ff627cdecfacb9dccc0c0a3be53feadf8bc138
Takos <takos2210@gmail.com>
2021-01-01 13:39:14 +0100

    Update verify.py

--- cogs/verify.py
+++ cogs/verify.py
@@ -11,7 +11,10 @@ class Verify(commands.Cog):
   @commands.Cog.listener()
   async def on_member_update(self, before: discord.Member, after: discord.Member):
         if not after.pending and before.pending:
-            await after.add_roles(discord.Object(id=612391389086351363))
+            return await after.add_roles(discord.Object(id=612391389086351363))
+        if (roles := (set(after.roles) - set(before.roles))):  # If there is a new role
+            if 605779930529136658 in [r.id for r in roles]:  # If lvl 1 is one of the roles
+                return await after.add_roles(discord.Object(id=612391389086351363))
 
 def setup(bot):
   bot.add_cog(Verify(bot))
