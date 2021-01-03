6432b4800735e744831fd5703d42debcdb85baf2
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-01-03 15:29:08 +0200

    whoops

--- cogs/qanda.py
+++ cogs/qanda.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 class QandA(commands.Cog):
   @commands.Cog.listener()
-  async def on_message(message):
+  async def on_message(self, message):
     role = discord.Object(id=795257055093981194)
     if message.channel.id == 795256498958630952:
       if role not in message.author.roles:
