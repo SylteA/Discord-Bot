b9b114daaba1cfba205e792dea0d4589cc82fc5c
mohamed040406 <55202038+mohamed040406@users.noreply.github.com>
2021-01-03 15:49:10 +0200

    whoops (#139)
    * fix
    
    Co-authored-by: Sylte <49946393+SylteA@users.noreply.github.com>

--- cogs/qanda.py
+++ cogs/qanda.py
@@ -1,13 +1,22 @@
-import discord
 from discord.ext import commands
+import discord
 
-class QandA(commands.Cog):
-  @commands.Cog.listener()
-  async def on_message(message):
-    role = discord.Object(id=795257055093981194)
-    if message.channel.id == 795256498958630952:
-      if role not in message.author.roles:
-        await message.author.add_roles(role)
 
 def setup(bot):
-  bot.add_cog(QandA())
+    bot.add_cog(QandA(bot=bot))
+
+
+class QandA(commands.Cog):
+    """Temporary cog for Q&A command handling!"""
+    
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message): -> None:
+        if message.channel =! 795256498958630952:
+            return
+        
+        role = discord.Object(id=795257055093981194)
+        if role not in message.author.roles:
+            await message.author.add_roles(role)
