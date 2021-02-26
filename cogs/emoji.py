77d6024febabe4f637a57e4bbfced8480e82a063
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2021-02-26 08:46:32 +0530

    Update emoji.py

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -1,1 +1,17 @@
+from discord.ext import commands
+import discord
 
+class Emoji(commands.Cog, name="emoji"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def cog_check(self, ctx):
+        if ctx.guild is None:
+            return False
+        return True
+      
+      
+      
+
+def setup(bot):
+    bot.add_cog(Emoji(bot=bot))
