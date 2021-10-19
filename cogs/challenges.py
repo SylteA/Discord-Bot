b29862a8e0383def01c00d493070d36fba8db1ef
classPythonAddike <72556571+classPythonAddike@users.noreply.github.com>
2021-10-19 18:19:32 +0530

    Add filetype to submission embeds in challenges

--- cogs/challenges.py
+++ cogs/challenges.py
@@ -35,17 +35,17 @@ class ChallengeHandler(commands.Cog):
     async def on_message(self, message):  # Submitted role.
 
         if message.channel.id in (680851820587122700, 713841306253656064):  # weekly
-            
+
             if message.author.id == self.bot.user.id:
                 return
-            
+
             if message.author.bot:
                 return await message.delete()
-            
-            if message.channel.id == 680851820587122700:  # weekly 1 
+
+            if message.channel.id == 680851820587122700:  # weekly 1
                 submitted = self.bot.guild.get_role(687417501931536478)
                 submission_channel = self.bot.guild.get_channel(729453161885990924)
-            
+
             else:  # weekly 2
                 submitted = self.bot.guild.get_role(715676464573317220)
                 submission_channel = self.bot.guild.get_channel(729453201081761862)
@@ -53,24 +53,39 @@ class ChallengeHandler(commands.Cog):
             if submitted not in message.author.roles:
                 await message.delete()
                 attach = message.attachments and message.attachments[0]
-                
+
                 if not attach:
-                    msg = f"{message.author.mention} make sure to __upload a " \
-                          f"python file__ that only includes the code required " \
-                          f"for the challenge!"
+                    msg = (
+                        f"{message.author.mention} make sure to __upload a "
+                        f"python file__ that only includes the code required "
+                        f"for the challenge!"
+                    )
                     return await message.channel.send(msg, delete_after=10.0)
-                
-                content = "```py\n" + (await attach.read()).decode("u8").replace("`", "\u200b`") + "```"
+
+                content = (
+                    "```py\n"
+                    + (await attach.read()).decode("u8").replace("`", "\u200b`")
+                    + "```"
+                )
                 if len(content) > 2040:
-                    msg = f"{message.author.mention} attachment can't be __more" \
-                          f" than 2040 characters__."
+                    msg = (
+                        f"{message.author.mention} attachment can't be __more"
+                        f" than 2040 characters__."
+                    )
                     return await message.channel.send(msg, delete_after=10.0)
 
                 await message.author.add_roles(submitted)
                 embed = discord.Embed(description=content, color=0x36393E)
-                embed.set_author(name=str(message.author), icon_url=message.author.avatar_url)
-                embed.set_footer(text=f'#ID: {message.author.id} • {len(content)-9} chars')
+                embed.set_author(
+                    name=str(message.author), icon_url=message.author.avatar_url
+                )
+                embed.set_footer(
+                    text=f'#ID: {message.author.id} • {len(content)-9} chars • Filetype: {attach.filename.split(".")[-1]}'
+                )
                 await submission_channel.send(embed=embed)
 
-        elif message.channel.id in [680851798340272141, 713841395965624490]:  # Automatic reaction
+        elif message.channel.id in [
+            680851798340272141,
+            713841395965624490,
+        ]:  # Automatic reaction
             await message.add_reaction("🖐️")
