72c74edb58d0b959f7c815bfc392c05bde4a3b81
SylteA <contact@sylte.dev>
2023-10-21 22:58:08 +0200

    Temporarily update models

--- bot/models/levelling_users.py
+++ bot/models/levelling_users.py
@@ -1,48 +1,8 @@
-from typing import Optional
-
 from .model import Model
 
 
-class Levels(Model):
+class LevellingUser(Model):
     id: int
-    rank: Optional[int]
     guild_id: int
     user_id: int
-    old_total_xp: Optional[int]
     total_xp: int
-
-    @classmethod
-    async def insert_by_guild(cls, guild_id: int, user_id: int, total_xp: int):
-        query = """INSERT INTO levelling_users (guild_id, user_id, total_xp)
-                        VALUES ($1, $2, $3)
-                   ON CONFLICT (guild_id, user_id)
-                     DO UPDATE
-                           SET total_xp = levelling_users.total_xp + $3, last_msg = CURRENT_TIMESTAMP
-                         WHERE levelling_users.guild_id = $1
-                           AND levelling_users.user_id = $2
-                   AND EXTRACT(EPOCH FROM (NOW() - levelling_users.last_msg)) > 60
-                     RETURNING total_xp - $3 AS old_total_xp, total_xp, guild_id, user_id, id;
-"""
-        return await cls.fetchrow(query, guild_id, user_id, total_xp)
-
-    @classmethod
-    async def give_xp(cls, guild_id: int, user_id: int, total_xp: int):
-        query = """INSERT INTO levelling_users (guild_id, user_id, total_xp)
-                        VALUES ($1, $2, $3)
-                    ON CONFLICT (guild_id, user_id)
-                  DO UPDATE SET total_xp = levelling_users.total_xp + $3
-                      RETURNING total_xp - $3 AS old_total_xp, total_xp, guild_id, user_id, id;
-                   """
-
-        return await cls.fetchrow(query, guild_id, user_id, total_xp)
-
-    @classmethod
-    async def remove_xp(cls, guild_id: int, user_id: int, total_xp: int):
-        query = """INSERT INTO levelling_users (guild_id, user_id, total_xp)
-                        VALUES ($1, $2, $3)
-                   ON CONFLICT (guild_id, user_id)
-                 DO UPDATE SET total_xp = levelling_users.total_xp - $3
-                     RETURNING total_xp + $3 AS old_total_xp, total_xp, guild_id, user_id, id;
-                   """
-
-        return await cls.fetchrow(query, guild_id, user_id, total_xp)
--- bot/models/persisted_role.py
+++ bot/models/persisted_role.py
@@ -1,29 +1,8 @@
-from typing import Optional
-
 from .model import Model
 
 
-class PersistentRole(Model):
+class PersistedRole(Model):
     id: int
     guild_id: int
     user_id: int
-    role_id: Optional[int]
-
-    @classmethod
-    async def list_by_guild(cls, guild_id: int, user_id: int):
-        query = """SELECT * FROM persisted_roles WHERE guild_id = $1 and user_id = $2"""
-        return await cls.fetch(query, guild_id, user_id)
-
-    @classmethod
-    async def insert_by_guild(cls, guild_id: int, user_id: int, role_id: int):
-        query = """INSERT INTO persisted_roles (guild_id, user_id, role_id)
-                        VALUES ($1, $2, $3)
-                   ON CONFLICT (guild_id, user_id, role_id)
-                   DO NOTHING"""
-
-        await cls.execute(query, guild_id, user_id, role_id)
-
-    @classmethod
-    async def delete_by_guild(cls, guild_id: int, user_id: int):
-        query = """DELETE FROM persisted_roles WHERE guild_id = $1 and user_id = $2"""
-        return await cls.execute(query, guild_id, user_id)
+    role_id: int
