bfed7d1f83e9b0d9ae7b20609f0bde4ffe3f5e1d
Sylte <contact@sylte.dev>
2023-11-06 22:53:25 +0100

    Update migration to make color an integer.

--- bot/models/migrations/005_down__custom_role.sql
+++ bot/models/migrations/005_down__custom_role.sql
@@ -1,1 +1,8 @@
 ALTER TABLE custom_roles DROP COLUMN user_id;
+
+BEGIN;
+ALTER TABLE custom_roles ADD COLUMN old_color VARCHAR NOT NULL;
+UPDATE custom_roles SET old_color = CAST(color AS VARCHAR);
+ALTER TABLE custom_roles DROP COLUMN color;
+ALTER TABLE custom_roles RENAME COLUMN old_color TO color;
+COMMIT;
