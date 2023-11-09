9a3e91e43353afe5ee25a35cefcbfa3ab67b236b
FirePlank <44502537+FirePlank@users.noreply.github.com>
2023-11-09 21:42:22 +0200

    Did the suggested changes

---
+++ bot/extensions/adventofcode/utils.py
@@ -0,0 +1,95 @@
+from datetime import datetime, timedelta
+from typing import Optional
+
+import aiohttp
+import discord
+import pytz
+from pydantic import BaseModel, validator
+
+from bot.config import settings
+
+YEAR = datetime.now(tz=pytz.timezone("EST")).year
+LEADERBOARD_ID = settings.aoc.leaderboard_id
+LEADERBOARD_CODE = settings.aoc.leaderboard_code
+API_URL = f"https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}.json"
+AOC_REQUEST_HEADER = {"user-agent": "Tech With Tim Discord Bot https://github.com/SylteA/Discord-Bot"}
+AOC_SESSION_COOKIE = {"session": settings.aoc.session_cookie}
+
+
+def home_embed():
+    embed = discord.Embed(title="", colour=0x68C290)
+    embed.set_author(
+        name="Advent of Code",
+        url="https://adventofcode.com",
+        icon_url="https://adventofcode.com/favicon.png",
+    )
+    embed.add_field(
+        name="Join the Leaderboard!",
+        value="Head over to https://adventofcode.com/leaderboard/private"
+        f" with the code `{LEADERBOARD_CODE}` to join the TWT private leaderboard!",
+        inline=False,
+    )
+
+    puzzle_time = next_puzzle()
+    if puzzle_time:
+        embed.add_field(
+            name="Next Puzzle",
+            value=f"The next puzzle is {discord.utils.format_dt(puzzle_time, 'R')}",
+            inline=False,
+        )
+
+    return embed
+
+
+def ordinal(n: int):
+    if 11 <= (n % 100) <= 13:
+        suffix = "th"
+    else:
+        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    return str(n) + suffix
+
+
+def next_puzzle():
+    now = datetime.now(tz=pytz.timezone("EST"))
+    if now.month == 12:
+        if now.day >= 25:
+            return False
+
+        # If it's December, calculate time until the next midnight EST time
+        target = now.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+    else:
+        # If it's not December, calculate time until the first of December
+        target = datetime(now.year, 12, 1, tzinfo=pytz.timezone("EST"))
+
+    return target
+
+
+class Member(BaseModel):
+    global_score: int
+    name: str
+    stars: int
+    last_star_ts: int
+    completion_day_level: dict
+    id: int
+    local_score: int
+
+    @validator("name", pre=True)
+    def set_name_to_anonymous(cls, val: Optional[str]) -> str:
+        if val is None:
+            return "Anonymous User"
+        return val
+
+
+async def fetch_leaderboard(local: bool = False) -> str:
+    url = f"https://adventofcode.com/{YEAR}/leaderboard"
+    if local:
+        url += f"/private/view/{LEADERBOARD_ID}.json"
+
+    async with aiohttp.ClientSession(cookies=AOC_SESSION_COOKIE, headers=AOC_REQUEST_HEADER) as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                response = await resp.text()
+            else:
+                resp.raise_for_status()
+
+    return response
---
+++ bot/extensions/adventofcode/views.py
@@ -0,0 +1,119 @@
+import json
+import re
+from datetime import datetime
+
+import discord
+import pytz
+from bs4 import BeautifulSoup
+from discord import ui
+
+from bot import core
+from bot.extensions.adventofcode.utils import LEADERBOARD_ID, YEAR, Member, fetch_leaderboard, home_embed, ordinal
+
+
+class CreateAdventOfCodeView(ui.View):
+    HOME_CUSTOM_ID = "extensions:adventofcode:home"
+    LOCAL_LEADERBOARD_CUSTOM_ID = "extensions:adventofcode:local"
+    GLOBAL_LEADERBOARD_CUSTOM_ID = "extensions:adventofcode:global"
+
+    def reset_buttons(self):
+        self.children[0].disabled = True
+        self.children[1].disabled = False
+        self.children[2].disabled = False
+
+    @discord.ui.button(label="Home", style=discord.ButtonStyle.gray, emoji="🏠", custom_id=HOME_CUSTOM_ID, disabled=True)
+    async def home(self, interaction: core.InteractionType, _button: ui.Button):
+        self.reset_buttons()
+        await interaction.response.edit_message(embed=home_embed(), view=self)
+
+    @discord.ui.button(
+        label="Local Leaderboard", style=discord.ButtonStyle.gray, emoji="👥", custom_id=LOCAL_LEADERBOARD_CUSTOM_ID
+    )
+    async def local_leaderboard(self, interaction: core.InteractionType, button: ui.Button):
+        leaderboard = await fetch_leaderboard(local=True)
+        leaderboard = json.loads(leaderboard)
+
+        members = [Member(**member_data) for member_data in leaderboard["members"].values()]
+
+        embed = discord.Embed(
+            title=f"{interaction.guild.name} Advent of Code Leaderboard",
+            colour=0x68C290,
+            url=f"https://adventofcode.com/{YEAR}/leaderboard/private/view/{LEADERBOARD_ID}",
+        )
+
+        leaderboard = {
+            "owner_id": leaderboard["owner_id"],
+            "event": leaderboard["event"],
+            "members": members,
+        }
+        members = leaderboard["members"]
+
+        for i, member in enumerate(sorted(members, key=lambda x: x.local_score, reverse=True)[:10], 1):
+            embed.add_field(
+                name=f"{ordinal(i)} Place: {member.name} ({member.stars} ⭐)",
+                value=f"Local Score: {member.local_score} | Global Score: {member.global_score}",
+                inline=False,
+            )
+        embed.set_footer(text=f"Current Day: {datetime.now(tz=pytz.timezone('EST')).day}/25")
+
+        button.disabled = True
+        self.children[0].disabled = False
+        self.children[2].disabled = False
+
+        await interaction.response.edit_message(embed=embed, view=self)
+        self.reset_buttons()
+
+    @discord.ui.button(
+        label="Global Leaderboard", style=discord.ButtonStyle.gray, emoji="🌎", custom_id=GLOBAL_LEADERBOARD_CUSTOM_ID
+    )
+    async def global_leaderboard(self, interaction: core.InteractionType, button: ui.Button):
+        raw_html = await fetch_leaderboard()
+        soup = BeautifulSoup(raw_html, "html.parser")
+
+        button.disabled = True
+        self.children[0].disabled = False
+        self.children[1].disabled = False
+
+        embed = discord.Embed(
+            title="Advent of Code Global Leaderboard",
+            colour=0x68C290,
+            url="https://adventofcode.com",
+        )
+
+        if soup.find("p").text == "Nothing to show on the leaderboard... yet.":
+            embed.description = "The global leaderboard is not available yet. Please try again later."
+            await interaction.response.edit_message(embed=embed, view=self)
+            return self.reset_buttons()
+
+        ele = soup.find_all("div", class_="leaderboard-entry")
+        exp = r"(?:[ ]{,2}(\d+)\))?[ ]+(\d+)\s+([\w\(\)\#\@\-\d ]+)"
+
+        lb_list = []
+        for entry in ele:
+            # Strip off the AoC++ decorator
+            raw_str = entry.text.replace("(AoC++)", "").rstrip()
+
+            # Group 1: Rank
+            # Group 2: Global Score
+            # Group 3: Member string
+            r = re.match(exp, raw_str)
+
+            rank = int(r.group(1)) if r.group(1) else None
+            global_score = int(r.group(2))
+
+            member = r.group(3)
+            if member.lower().startswith("(anonymous"):
+                # Normalize anonymous user string by stripping () and title casing
+                member = re.sub(r"[\(\)]", "", member).title()
+
+            lb_list.append((rank, global_score, member))
+
+        s_desc = "\n".join(
+            f"`{index}` {lb_list[index - 1][2]} - {lb_list[index - 1][1]} "
+            for index, title in enumerate(lb_list[:10], start=1)
+        )
+
+        embed.description = s_desc
+
+        await interaction.response.edit_message(embed=embed, view=self)
+        self.reset_buttons()
