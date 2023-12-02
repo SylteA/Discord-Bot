73a41659df403f11abe21e60681caeaf4ca58a64
Sarthak singh <sarthaksingh2001@gmail.com>
2023-12-02 18:31:16 +0530

    syntax fix, oopsie

--- bot/models/migrations/006_up__configs.sql
+++ bot/models/migrations/006_up__configs.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS configs
 (
     id                          BIGINT PRIMARY KEY DEFAULT create_snowflake(),
-    guild_id                    BIGINT NOT NULL,
+    guild_id                    BIGINT UNIQUE NOT NULL,
     min_xp                      INT NOT NULL DEFAULT 15,
     max_xp                      INT NOT NULL DEFAULT 25,
     xp_boost                    INT NOT NULL DEFAULT 1,
-    custom_role_log_channel_id  BIGINT,
+    custom_role_log_channel_id  BIGINT
 );
