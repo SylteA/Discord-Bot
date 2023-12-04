9ae9dec987f13006e4905a4037bc94a6b6f74ec8
Hamza <hamzaoubrahim51@gmail.com>
2023-12-04 13:12:31 +0100

    requested changes resolved

--- utils/health.py
+++ utils/health.py
@@ -1,4 +1,10 @@
+import os
 
-def update_health(name: str, value):
-    with open(f'/tmp/.health.{name.lower()}', 'w') as f:
+def update_health(name: str, value: bool):
+    path = f'/tmp/.health.{name.lower()}'
+    
+    with open(path, 'w') as f:
         f.write(str(value))
+    # checking after to avoid making sure the file exists
+    if not value:
+        os.remove(path)
