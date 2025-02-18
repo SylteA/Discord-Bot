2c6801eece4ceec5657e5fba2170bea89653191d
HETHAT <zhethat@gmail.com>
2025-02-18 15:57:34 +0100

    Remove ClashOfCodeEvents cog

--- bot/extensions/clashofcode/__init__.py
+++ bot/extensions/clashofcode/__init__.py
@@ -2,11 +2,9 @@ from bot.config import settings
 from bot.core import DiscordBot
 
 from .commands import ClashOfCode
-from .events import ClashOfCodeEvents
 from .utils import coc_client
 
 
 async def setup(bot: DiscordBot) -> None:
     await coc_client.login(remember_me_cookie=settings.coc.session_cookie)
     await bot.add_cog(ClashOfCode(bot=bot))
-    await bot.add_cog(ClashOfCodeEvents(bot=bot))
